0372c9d3773a6efbf179b770287297db2e519527
NES <nspry1997@gmail.com>
2020-07-21 15:16:16 -0400

    prepared for deployment

---
+++ client/build/precache-manifest.4a8a4f71fa320e9e964d6775e1f6c11f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caaf7e086a81df634f931b1690b6516b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e05f4fec21bdf9730406",
+    "url": "/static/css/main.bccd0344.chunk.css"
+  },
+  {
+    "revision": "38247fb6dbacb8c36e68",
+    "url": "/static/js/2.baca2bfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.baca2bfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e05f4fec21bdf9730406",
+    "url": "/static/js/main.8e4ca5b8.chunk.js"
+  },
+  {
+    "revision": "77e668e81262a3b42984",
+    "url": "/static/js/runtime-main.b3e5a6b4.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dontenv = require('dotenv')
 const colors = require('colors')
@@ -13,8 +14,17 @@ const transactions = require('./routes/transactionsRoute')
 const app = express()
 // Body Parse Middleware
 app.use(express.json())
+// Morgan
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 // Use Routes
 app.use('/api/v1/transactions', transactions)
+// Build
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 // Port and Server Listening
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server started in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
